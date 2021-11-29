2ce120ec948c62cf3661f046f2467080e9eb22e4
satheesh nayaka k a <sathishadipad1998@gmail.com>
2021-11-29 19:41:14 +0530

    pass

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
     // Write your code here
+    const obj={};
+    let i=0;
+    while(i<(arr.length)){
+        obj[arr[i]]=arr[i+1];
+        i=i+2
+    }
+    return obj
 }
 
 module.exports = CreateObject;
